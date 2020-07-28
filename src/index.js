547f8743f75fd2fb8c5ba11731d6118b2e87b4ee
cmnewcomer <cmnewcomer@gmail.com>
2020-07-28 12:07:00 +0200

    Updated Current Weather Description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,94 @@
+// Current Longetude & Latitude Temperature
+function showPosition(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showCurrentCityTemperature);
+}
+
+function showCurrentCityTemperature(response) {
+  console.log(response.data.weather[0].description);
+  let temperature = Math.round(response.data.main.temp);
+  let currentCity = response.data.name;
+  let displayTemp = document.querySelector("#currentTemp");
+  let displayCity = document.querySelector("#cityName");
+  displayTemp.innerHTML = `${temperature}º`;
+  displayCity.innerHTML = `${currentCity}`;
+  let tempMin = Math.round(response.data.main.temp_min);
+  let tempMax = Math.round(response.data.main.temp_max);
+  console.log(tempMin);
+  console.log(tempMax);
+  let tempMinElement = document.querySelector("#low");
+  let tempMaxElement = document.querySelector("#high");
+  console.log(tempMaxElement);
+  console.log(tempMinElement);
+  tempMinElement.innerHTML = `${tempMin}`;
+  tempMaxElement.innerHTML = `${tempMax}`;
+
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#currentWeatherDescription");
+  descriptionElement.innerHTML = `${description}`;
+}
+
+function currentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocalTemp = document.querySelector("#currentLocation");
+currentLocalTemp.addEventListener("click", currentPosition);
+
+navigator.geolocation.getCurrentPosition(showPosition);
+
+//feature search engine, replace city name and city's current temperature
+function searchCity(event) {
+  event.preventDefault();
+  let citySearchInput = document.querySelector("#city-text-input");
+  let city = document.querySelector("#cityName");
+  city.innerHTML = `${citySearchInput.value}`;
+  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&units=metric`;
+  console.log(citySearchInput.value);
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showCityTemperatureC);
+}
+
+//temperature in celcius
+function showCityTemperatureC(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#currentTemp");
+  temperatureElement.innerHTML = `${temperature}`;
+
+  let tempMin = Math.round(response.data.main.temp_min);
+  let tempMax = Math.round(response.data.main.temp_max);
+  let tempMinElement = document.querySelector("#low");
+  let tempMaxElement = document.querySelector("#high");
+  tempMinElement.innerHTML = `${tempMin}`;
+  tempMaxElement.innerHTML = `${tempMax}`;
+
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#currentWeatherDescription");
+  descriptionElement.innerHTML = `${description}`;
+}
+
+//temperature in Farenheit
+
+function searchCityFTemp(event) {
+  event.preventDefault();
+  let citySearchInput = document.querySelector("#city-text-input");
+  let city = document.querySelector("#cityName");
+  city.innerHTML = `${citySearchInput.value}`;
+  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&units=imperial`;
+  console.log(citySearchInput.value);
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showCityTemperatureF);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
 //feature 1 display current date and time
 
 function formDate() {
@@ -48,92 +139,3 @@ let currentDate = document.querySelector("#currentDate");
 let currentTime = document.querySelector("#currentTime");
 currentDate.innerHTML = formDate();
 currentTime.innerHTML = formTime();
-
-//feature search engine, replace city name and citie's current temperature
-function searchCity(event) {
-  event.preventDefault();
-  let citySearchInput = document.querySelector("#city-text-input");
-  let city = document.querySelector("#cityName");
-  city.innerHTML = `${citySearchInput.value}`;
-  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&units=metric`;
-  console.log(citySearchInput.value);
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showCityTemperatureC);
-}
-
-//temperature in celcius
-function showCityTemperatureC(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#currentTemp");
-  console.log(temperatureElement);
-  temperatureElement.innerHTML = `${temperature}`;
-  let tempMin = Math.round(response.data.main.temp_min);
-  let tempMax = Math.round(response.data.main.temp_max);
-  console.log(tempMin);
-  console.log(tempMax);
-  let tempMinElement = document.querySelector("#low");
-  let tempMaxElement = document.querySelector("#high");
-  console.log(tempMaxElement);
-  console.log(tempMinElement);
-  tempMinElement.innerHTML = `${tempMin}`;
-  tempMaxElement.innerHTML = `${tempMax}`;
-}
-
-//temperature in Farenheit
-
-function searchCityFTemp(event) {
-  event.preventDefault();
-  let citySearchInput = document.querySelector("#city-text-input");
-  let city = document.querySelector("#cityName");
-  city.innerHTML = `${citySearchInput.value}`;
-  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&units=imperial`;
-  console.log(citySearchInput.value);
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showCityTemperatureF);
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchCity);
-
-// Current Longetude & Latitude Temperature
-function showPosition(position) {
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
-  let apiKey = "fbbef86de25dd6b2558fa7cb141039b2";
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(showCurrentCityTemperature);
-}
-
-function showCurrentCityTemperature(response) {
-  console.log(response.data.main.temp);
-  let temperature = Math.round(response.data.main.temp);
-  let currentCity = response.data.name;
-  let displayTemp = document.querySelector("#currentTemp");
-  let displayCity = document.querySelector("#cityName");
-  displayTemp.innerHTML = `${temperature}º`;
-  displayCity.innerHTML = `${currentCity}`;
-  let tempMin = Math.round(response.data.main.temp_min);
-  let tempMax = Math.round(response.data.main.temp_max);
-  console.log(tempMin);
-  console.log(tempMax);
-  let tempMinElement = document.querySelector("#low");
-  let tempMaxElement = document.querySelector("#high");
-  console.log(tempMaxElement);
-  console.log(tempMinElement);
-  tempMinElement.innerHTML = `${tempMin}`;
-  tempMaxElement.innerHTML = `${tempMax}`;
-}
-
-function currentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-let currentLocalTemp = document.querySelector("#currentLocation");
-currentLocalTemp.addEventListener("click", currentPosition);
-
-navigator.geolocation.getCurrentPosition(showPosition);
